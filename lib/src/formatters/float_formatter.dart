06999501ac95a40fdec9436591168bf8862d34c8
Herman Bergwerf <hermanbergwerf@gmail.com>
2020-04-22 00:59:05 +0200

    Remove TODO notice

--- lib/src/formatters/float_formatter.dart
+++ lib/src/formatters/float_formatter.dart
@@ -1,7 +1,6 @@
 part of sprintf;
 
 class FloatFormatter extends Formatter {
-  // TODO: can't rely on '.' being the decimal separator
   static final _number_rx = new RegExp(r'^[\-\+]?(\d+)(?:\.(\d+))?$');
   static final _expo_rx = new RegExp(r'^[\-\+]?(\d)\.(\d+)e([\-\+]?\d+)$');
   static final _leading_zeroes_rx = new RegExp(r'^(0*)[1-9]+');
